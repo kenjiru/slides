14c8c3f3470297421b974eb5ffda89e92c6c2945
kenjiru <kenjiru.ro@gmail.com>
2013-06-09 22:04:33 +0200

    Refactored the Navigation class to make some methods public.

--- modules/src/slides/js/navigation.js
+++ modules/src/slides/js/navigation.js
@@ -5,59 +5,74 @@ var Navigation = Y.Base.create('navigation', Y.Base, [], {
     initializer : function() {
         // IE<9 need to listen to key event on the document
         Y.one(document).on('keydown', Y.bind(this._handleKeyPress, this));
-        Y.on('domready', Y.bind(this._handleDomReady, this));
+
+        this.publish('changed');
+
+        this._initializeSections();
     },
 
-    _handleDomReady : function() {
+    _initializeSections : function() {
         this._sections = Y.all('section');
 
         this._sections.addClass('future');
 
-        this._getCurrentSection().removeClass('future');
-        this._getCurrentSection().addClass('present');
+        this._getCurrent().removeClass('future');
+        this._getCurrent().addClass('present');
     },
 
     _handleKeyPress : function(ev) {
         if (ev.keyCode == 37) {
-            this._showPreviousSlide();
+            this.showPrevious();
         } else if (ev.keyCode == 39) {
-            this._showNextSlide();
+            this.showNext();
         }
     },
 
-    _showPreviousSlide : function() {
-        if (this._currentIndex > 0) {
-            this._getCurrentSection().removeClass('present');
-            this._getCurrentSection().addClass('future');
+    showPrevious : function() {
+        if (this.hasPrevious()) {
+            this._getCurrent().removeClass('present');
+            this._getCurrent().addClass('future');
 
-            this._getPreviousSection().removeClass('past');
-            this._getPreviousSection().addClass('present');
+            this._getPrevious().removeClass('past');
+            this._getPrevious().addClass('present');
 
             this._currentIndex -= 1;
+
+            this.fire('changed');
         }
     },
 
-    _showNextSlide : function() {
-        if (this._currentIndex < this._sections.size() - 1) {
-            this._getCurrentSection().removeClass('present');
-            this._getCurrentSection().addClass('past');
+    showNext : function() {
+        if (this.hasNext()) {
+            this._getCurrent().removeClass('present');
+            this._getCurrent().addClass('past');
 
-            this._getNextSection().removeClass('future');
-            this._getNextSection().addClass('present');
+            this._getNext().removeClass('future');
+            this._getNext().addClass('present');
 
             this._currentIndex += 1;
+
+            this.fire('changed');
         }
     },
 
-    _getCurrentSection : function() {
+    _getCurrent : function() {
         return this._sections.item(this._currentIndex);
     },
 
-    _getPreviousSection : function() {
+    _getPrevious : function() {
         return this._sections.item(this._currentIndex - 1);
     },
 
-    _getNextSection : function() {
+    _getNext : function() {
         return this._sections.item(this._currentIndex + 1);
+    },
+
+    hasNext : function() {
+        return this._currentIndex < this._sections.size() - 1;
+    },
+
+    hasPrevious : function() {
+        return this._currentIndex > 0;
     }
 });
