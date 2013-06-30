45af4bbca0d16f644de24a3e0c658cddea9ae215
kenjiru <kenjiru.ro@gmail.com>
2013-06-30 15:57:04 +0200

    Added the up and down controls.

--- src/gallery-slides/js/navigation-controls.js
+++ src/gallery-slides/js/navigation-controls.js
@@ -2,6 +2,8 @@ Y.namespace('slides').NavigationControls = Y.Base.create('navigationControls', Y
     CONTENT_TEMPLATE : null,
     _moveRightButton : null,
     _moveLeftButton : null,
+    _moveUpButton : null,
+    _moveDownButton : null,
 
     initializer : function() {
         this._moveRightButton = new NavigationButton({
@@ -16,7 +18,21 @@ Y.namespace('slides').NavigationControls = Y.Base.create('navigationControls', Y
             checkCallback : Y.bind(Y.navigation.hasLeft, Y.navigation)
         });
 
+        this._moveUpButton = new NavigationButton({
+            name : 'up',
+            clickCallback : Y.bind(Y.navigation.showUp, Y.navigation),
+            checkCallback : Y.bind(Y.navigation.hasUp, Y.navigation)
+        });
+
+        this._moveDownButton = new NavigationButton({
+            name : 'down',
+            clickCallback : Y.bind(Y.navigation.showDown, Y.navigation),
+            checkCallback : Y.bind(Y.navigation.hasDown, Y.navigation)
+        });
+
         this.add(this._moveRightButton);
         this.add(this._moveLeftButton);
+        this.add(this._moveUpButton);
+        this.add(this._moveDownButton);
     }
 });
