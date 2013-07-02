8be9d0f8657702d6923073128fe6c0a26f14f22e
kenjiru <kenjiru.ro@gmail.com>
2013-07-03 00:17:13 +0200

    Added the ProgressBar widget.

---
+++ src/gallery-slides/js/progress-bar.js
@@ -0,0 +1,52 @@
+Y.namespace('slides').ProgressBar = Y.Base.create('progressBar', Y.Widget, [], {
+    CONTENT_TEMPLATE : "<span></span>",
+    _totalSections : 0,
+    _currentIndex : 0,
+    _sectionIndexArray : [],
+
+    initializer : function() {
+        this._findTotalSlides();
+    },
+
+    _findTotalSlides : function() {
+        var sections = Y.all('.slides>section'),
+            total = 0;
+
+        this._sectionIndexArray[0] = 0;
+
+        sections.each(function(section, i) {
+            if (section.hasClass('stack')) {
+                total += section.all('section').size();
+
+                this._sectionIndexArray[i+1] = total;
+            } else {
+                this._sectionIndexArray[i+1] = ++total;
+            }
+        }, this);
+
+        this._totalSections = total;
+    },
+
+    renderUI : function() {
+        this._updateLayout();
+    },
+
+    bindUI : function() {
+        Y.navigation.on('navigation:changed', this._updateProgress, this);
+        Y.on('windowresize', Y.bind(this._updateLayout, this));
+    },
+
+    _updateProgress : function() {
+        var current = Y.navigation.getCurrentIndex();
+
+        this._currentIndex = this._sectionIndexArray[current.index] + current.subIndex;
+        this._updateLayout();
+    },
+
+    _updateLayout : function() {
+        var boundingWidth = this.get('boundingBox').get('offsetWidth'),
+            sectionWidth = boundingWidth / (this._totalSections - 1);
+
+        this.get('contentBox').setStyle('width', this._currentIndex * sectionWidth);
+    }
+});
--- src/gallery-slides/js/slides.js
+++ src/gallery-slides/js/slides.js
@@ -2,6 +2,6 @@ Y.on('domready', function(){
     Y.navigation = new Y.slides.Navigation();
     // Navigation sets all the CSS classes, and Layout needs to know about some classes
     new Y.slides.Layout();
-
     new Y.slides.NavigationControls().render();
+    new Y.slides.ProgressBar().render();
 });
