b4faa77cc9b37bd21fb76ccfe4a3f6f4088fe465
kenjiru <kenjiru.ro@gmail.com>
2013-07-05 01:07:59 +0200

    Tentative implementation of the enable overview method.

--- src/gallery-slides/js/overview.js
+++ src/gallery-slides/js/overview.js
@@ -21,15 +21,61 @@ Y.namespace('slides').Overview = Y.Base.create('overview', Y.Base, [], {
         }
     },
 
-    _disableOverview : function() {
-        console.log('overview disabled!');
+    _enableOverview : function() {
+        var slidesContainer = Y.one('.slides-container'),
+            sections = Y.all('.slides>section'),
+            current = Y.navigation.getCurrentIndex(),
+            noSections = sections.size();
 
-        this._overviewEnabled = false;
+        slidesContainer.addClass('overview');
+
+        sections.each(function(section, i){
+            var xTranslate = (i - current.index) * 105,
+                transformStr = 'translateZ(-2500px) translate(' + xTranslate + '%, 0%)'
+
+            this._setTransform(section, transformStr);
+
+            if(section.hasClass('stack')) {
+                var subSections = section.all('section'),
+                    subIndex = section.getAttribute('subIndex') || '0';
+
+                subIndex = parseInt(subIndex, 10);
+
+                subSections.each(function(subSection, i) {
+                    var yTranslate = (i - subIndex) * 105,
+                        translateStr = 'translate(0%, ' + yTranslate + '%)';
+
+                    this._setTransform(subSection, translateStr);
+
+                    subSection.setStyle('top', '-340px');
+                }, this);
+
+                section.setStyle('top', '0');
+            } else {
+                section.setStyle('top', '-340px');
+            }
+        }, this);
+
+        console.log('overview enabled!');
+        this._overviewEnabled = true;
     },
 
-    _enableOverview : function() {
-        console.log('overview enabled!');
+    _setTransform : function(node, transformStr) {
+        node.setStyles({
+            '-webkit-transform' : transformStr,
+            '-ms-transform' : transformStr,
+            '-o-transform' : transformStr,
+            'transform' : transformStr
+        });
 
-        this._overviewEnabled = true;
+    },
+
+    _disableOverview : function() {
+        var slidesContainer = Y.one('.slides-container');
+
+        slidesContainer.removeClass('overview');
+
+        console.log('overview disabled!');
+        this._overviewEnabled = false;
     }
 });
